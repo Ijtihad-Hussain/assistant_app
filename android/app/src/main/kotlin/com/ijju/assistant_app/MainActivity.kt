7e886b7d496867645c7d0a85e5f8538530335772
Ijtihad Hussain <mr.ijtihadhussain@gmail.com>
2023-09-04 10:17:05 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/ijju/assistant_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ijju.assistant_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/secrets.dart
@@ -0,0 +1,2 @@
+
+const openAIAPIKey = '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';
